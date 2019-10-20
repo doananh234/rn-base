a73851ee57e2b0bb6ce2467c2d5f5c05746d64b4
Anh <doananh234@gmail.com>
2019-10-20 13:34:26 +0700

    update + fix for android

---
+++ apps/mobile-app/ios/rnBase/AppDelegate.m
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "AppDelegate.h"
+
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <CodePush/CodePush.h>
+#import <React/RCTLinkingManager.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"rnBase"
+                                            initialProperties:nil];
+
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  //config for facebook
+//  [[FBSDKApplicationDelegate sharedInstance] application:application
+//                           didFinishLaunchingWithOptions:launchOptions];
+  return YES;
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  return [CodePush bundleURL];
+#endif
+}
+
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  //handle open facebook
+//  BOOL handledFB = [[FBSDKApplicationDelegate sharedInstance] application:application
+//                                                                  openURL:url
+//                                                        sourceApplication:sourceApplication
+//                                                               annotation:annotation
+//                    ];
+  // handle open deep-link or link
+  BOOL handledRCT = [RCTLinkingManager application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+  
+//  return handledFB || handledRCT;
+  return handledRCT;
+}
+
+// Only if your app is using [Universal Links](https://developer.apple.com/library/prerelease/ios/documentation/General/Conceptual/AppSearch/UniversalLinks.html).
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity
+ restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler
+{
+  // handle open deep-link or link
+  return [RCTLinkingManager application:application
+                   continueUserActivity:userActivity
+                     restorationHandler:restorationHandler];
+}
+
+@end
--- apps/mobile-app/src/Setup.js
+++ apps/mobile-app/src/Setup.js
@@ -1,37 +1,25 @@
 import React from 'react';
-import {
-  Text,
-} from 'react-native';
-import {
-  PersistGate,
-} from 'redux-persist/es/integration/react';
+import {Text} from 'react-native';
+import {PersistGate} from 'redux-persist/es/integration/react';
 
-import {
-  Provider,
-} from 'react-redux';
-import {
- useScreens,
-} from 'react-native-screens';
+import {Provider} from 'react-redux';
+import {useScreens} from 'react-native-screens';
 import configI18n from './i18n/index';
 import configureStore from './redux/store';
 import './themes/Images';
 import AppNavigation from './navigation/AppNavigation';
-import {
-  setNavigator,
-} from './navigation/NavigatorService';
+import {setNavigator} from './navigation/NavigatorService';
 
 // TODO: Enable screens support before any of your navigation screen renders
 useScreens();
 
-const { persistor, store } = configureStore(stored => {
+const {persistor, store} = configureStore(stored => {
   configI18n(stored);
 });
 
-
-const Loading = () => (<Text>Loading</Text>);
+const Loading = () => <Text>Loading</Text>;
 
 export default function Setup() {
-
   const onBeforeLift = () => {
     // take some action before the gate lifts
   };
@@ -41,14 +29,14 @@ export default function Setup() {
       <PersistGate
         loading={<Loading />}
         onBeforeLift={onBeforeLift}
-        persistor={persistor}
-      >
-        <AppNavigation ref={nav => {
-          // TODO: Setup navigator to use react-navigation in saga
-          // eslint-disable-next-line max-len
-          // Notice: useRef & setNavigator in useEffect not work with PersistGate
-          setNavigator(nav);
-        }}
+        persistor={persistor}>
+        <AppNavigation
+          ref={nav => {
+            // TODO: Setup navigator to use react-navigation in saga
+            // eslint-disable-next-line max-len
+            // Notice: useRef & setNavigator in useEffect not work with PersistGate
+            setNavigator(nav);
+          }}
         />
       </PersistGate>
     </Provider>
